123e1fb2f4fed09f3206cd796754753c20f0e464
Vaibhav Mule <vaibhavmule135@gmail.com>
2016-01-08 11:52:34 +0530

    add session mappign #34

--- backend/mapping/event.js
+++ backend/mapping/event.js
@@ -1,47 +1,10 @@
 {
   "mappings": {
-    "events": {
+    "sessions": {
       "properties": {
         "english": {
           "properties": {
-            "city": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "classification": {
-              "type": "string",
-              "index": "not_analyzed",
-              "analyzer": "english"
-            },
-            "country": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "description": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "endingDate": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "keywords": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "languages": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "startingDate": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "startingTime": {
-              "type": "string",
-              "analyzer": "english"
-            },
-            "state": {
+            "number": {
               "type": "string",
               "analyzer": "english"
             },
@@ -49,11 +12,23 @@
               "type": "string",
               "analyzer": "english"
             },
-            "translation": {
+            "venue": {
               "type": "string",
               "analyzer": "english"
             },
-            "venue": {
+            "classification": {
+              "type": "string",
+              "index": "not_analyzed"
+            },
+            "startingDate": {
+              "type": "date",
+              "analyzer": "english"
+            },
+            "languages": {
+              "type": "string",
+              "analyzer": "english"
+            },
+            "translation": {
               "type": "string",
               "analyzer": "english"
             }
@@ -61,44 +36,7 @@
         },
         "french": {
           "properties": {
-            "city": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "classification": {
-              "type": "string",
-              "analyzer": "french",
-              "index": "not_analyzed"
-            },
-            "country": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "description": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "endingDate": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "keywords": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "languages": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "startingDate": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "startingTime": {
-              "type": "string",
-              "analyzer": "french"
-            },
-            "state": {
+            "number": {
               "type": "string",
               "analyzer": "french"
             },
@@ -106,11 +44,23 @@
               "type": "string",
               "analyzer": "french"
             },
-            "translation": {
+            "venue": {
               "type": "string",
               "analyzer": "french"
             },
-            "venue": {
+            "classification": {
+              "type": "string",
+              "analyzer": "french"
+            },
+            "startingDate": {
+              "type": "string",
+              "analyzer": "french"
+            },
+            "languages": {
+              "type": "string",
+              "analyzer": "french"
+            },
+            "translation": {
               "type": "string",
               "analyzer": "french"
             }
