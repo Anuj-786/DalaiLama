22b3f0680686657a58a3d7bcac958ac71770fdc7
Vaibhav Mule <vaibhavmule135@gmail.com>
2016-01-08 11:57:14 +0530

    #34 add person mapping

---
+++ backend/mapping/person.js
@@ -0,0 +1,24 @@
+{
+  "mappings": {
+    "persons": {
+      "properties": {
+        "english": {
+          "properties": {
+            "name": {
+              "type": "string",
+              "analyzer": "english"
+            }
+          }
+        },
+        "french": {
+          "properties": {
+            "name": {
+              "type": "string",
+              "analyzer": "french"
+            }
+          }
+        }
+      }
+    }
+  }
+}
