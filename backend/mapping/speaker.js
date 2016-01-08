37f5bd9dee557904354bfac25b2bb90d34bf5a52
Vaibhav Mule <vaibhavmule135@gmail.com>
2016-01-08 12:00:06 +0530

    add speaker mapping #34

---
+++ backend/mapping/speaker.js
@@ -0,0 +1,32 @@
+{
+  "mappings": {
+    "speakers": {
+      "properties": {
+        "english": {
+          "properties": {
+            "type": {
+              "type": "string",
+              "analyzer": "english"
+            },
+            "language": {
+              "type": "string",
+              "analyzer": "english"
+            }
+          }
+        },
+        "french": {
+          "properties": {
+            "type": {
+              "type": "string",
+              "analyzer": "french"
+            },
+            "language": {
+              "type": "string",
+              "analyzer": "french"
+            }
+          }
+        }
+      }
+    }
+  }
+}
