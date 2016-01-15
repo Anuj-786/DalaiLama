cd8af86eecfc68668032b1c3c8b7fcad41d3611a
Vaibhav Mule <vaibhavmule135@gmail.com>
2016-01-15 10:09:53 +0530

    add hard disk sync code

--- backend/app/hard-disk/sync.js
+++ backend/app/hard-disk/sync.js
@@ -1,16 +1,52 @@
 var debug = require('debug')('sync')
 var fs = require('fs')
 
-var walk = require('walk-promise');
-var exif = require('exiftool');
+var walk = require('walk-promise')
+var exif = require('exiftool')
 var _ = require('lodash')
 
-/*
-walk('/media/vaibhavmule/Untitled/2015-10-10-dharamsala-petonschool-edit')
+
+walk('/media/vaibhavmule/Untitled/OHHDL_images')
   .then(function(files) {
     _.forEach(files, function(file) {
+
+      if (file.name.charAt(0) === '.') {
+        return
+      }
+
       debug(file.root + '/' + file.name, 'file')
-      fs.readFileAsync(file.root + '/' + file.name, function(err, data) {
+
+      var buffer = new Buffer(1024)
+
+      debug('buffer', buffer)
+
+      var rstream = fs.createReadStream(file.root + '/' + file.name, {
+        encoding: null,
+        start: 0,
+        end: 1024 * 1024
+      })
+
+      rstream
+        .on('data', function(chunck) {
+          debug(chunck)
+          buffer += chunck
+        })
+        .on('end', function() { // done
+          
+          exif.metadata(buffer, function(err, metadata) {
+            if (err)
+              throw err;
+            else
+              debug(metadata, 'metadata');
+          })
+        })
+    })
+  }).catch(function(err) {
+    debug(err, 'err')
+  })
+
+/*
+      fs.read(file.root + '/' + file.name, function(err, data) {
         if (err)
           throw err;
         else {
@@ -22,37 +58,31 @@ walk('/media/vaibhavmule/Untitled/2015-10-10-dharamsala-petonschool-edit')
           })
         }
       })
-    })
-  }).catch(function(err) {
-    debug(err, 'err')
-  })*/
-
-var root = '/media/vaibhavmule/Untitled/2015-10-10-dharamsala-petonschool-edit/'
-var file = '2015-10-10-dharamsala-petonschool-tibetan-audio.mp3'
+*/
 
 /*
-.DS_Store
-._.DS_Store
-  ._2015-10-10-dharamsala-petonschool-tibetan-audio-export.wav
-  ._2015-10-10-dharamsala-petonschool-tibetan-video-export.mov
-  2015-10-10-dharamsala-petonschool-tibetan-audio-edit.wav 
-  2015-10-10-dharamsala-petonschool-tibetan-audio-export.wav 
-  2015-10-10-dharamsala-petonschool-tibetan-audio.mp3 
-  2015-10-10-dharamsala-petonschool-tibetan-video-HD.mp4 
-  2015-10-10-dharamsala-petonschool-tibetan-video-export.mov 
-  2015-10-10-dharamsala-petonschool-tibetan-video-hq.mp4 
-  */
+      var buffer = new Buffer(1024);
+
+      fs.open(file.root + '/' + file.name, 'r+', function(err, fd) {
+        if (err) {
+          debug(err)
+          throw err
+        }
+
+        fs.read(fd, buffer, 0, buffer.length, 0, function(err, bytes, buff) {
+          if (err) {
+            debug('error', err);
+          }
+          debug(bytes, buffer)
+          exif.metadata(buffer, function(err, metadata) {
+            if (err) {
+              throw err;
+            } else
+              debug(metadata, 'metadata')
+          })
+
+        })
 
 
-fs.readFile(root + file, function (err, data) {
-  if (err)
-    throw err;
-  else {
-    exif.metadata(data, function (err, metadata) {
-      if (err)
-        throw err;
-      else
-        console.log(metadata);
-    });
-  }
-});
+      })
+*/
